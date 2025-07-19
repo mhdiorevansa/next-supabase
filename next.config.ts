23651f0623b48e453ac60ec0ccac7072873f6be1
Muhammad Dio Revansa <mhdiorevansa098@gmail.com>
2025-07-19 12:50:08 +0700

    add domain image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	images: {
+		domains: ["encrypted-tbn0.gstatic.com", "cdn0-production-images-kly.akamaized.net"],
+	},
 };
 
 export default nextConfig;
