42b4ae6fbbb5b7b900ce5d133d5bc8047b6a333e
Muhammad Dio Revansa <mhdiorevansa098@gmail.com>
2025-07-24 14:18:12 +0700

    feat: add new feature registration user

---
+++ src/app/register/page.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { FormEvent, useState } from "react";
+import Link from "next/link";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import createBrowserSupabaseClient from "@/lib/supabase/client/browser-client";
+
+const RegisterPage = () => {
+	const router = useRouter();
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [loading, setLoading] = useState(false);
+
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setLoading(true);
+		const { error } = await createBrowserSupabaseClient.auth.signUp({
+			email,
+			password,
+		});
+		setLoading(false);
+		if (password.length < 6) {
+			toast.error("Password minimal 6 karakter");
+			return;
+		}
+		if (error) {
+			console.log(error);
+			toast.error("Sepertinya ada kesalahan");
+		} else {
+			toast.success("Berhasil register");
+			router.push("/admin");
+		}
+	};
+
+	return (
+		<div className="min-h-screen flex items-center justify-center px-4">
+			<Card className="w-full max-w-md shadow-xl">
+				<CardHeader>
+					<CardTitle className="text-center text-2xl font-bold">Registrasi Akun Anda</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<form onSubmit={handleSubmit} className="space-y-4">
+						<div className="space-y-2">
+							<Label htmlFor="email">Email</Label>
+							<Input
+								id="email"
+								type="email"
+								placeholder="you@example.com"
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+								required
+							/>
+						</div>
+						<div className="space-y-2">
+							<Label htmlFor="password">Password</Label>
+							<Input
+								id="password"
+								type="password"
+								placeholder="••••••••"
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+								required
+							/>
+						</div>
+						<Button
+							type="submit"
+							className={`w-full ${loading ? "cursor-no-drop" : "cursor-pointer"}`}
+							disabled={loading}>
+							{loading ? "Loading..." : "Register"}
+						</Button>
+					</form>
+					<p className="mt-4 text-center text-sm text-muted-foreground">
+						Sudah punya akun?{" "}
+						<Link href="/login" className="text-primary underline">
+							Login sekarang
+						</Link>
+					</p>
+				</CardContent>
+			</Card>
+		</div>
+	);
+};
+
+export default RegisterPage;
